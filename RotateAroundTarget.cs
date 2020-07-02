24d735fe580be7e377dad1338f95c3f85b4e0dd0
Ender Yılmaz <49018223+enderylmz@users.noreply.github.com>
2020-07-02 15:55:12 +0300

    Update RotateAroundTarget.cs

--- RotateAroundTarget.cs
+++ RotateAroundTarget.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scrRotateAroundTarget : MonoBehaviour
+public class RotateAroundTarget : MonoBehaviour
 {
  
     public Transform target;
